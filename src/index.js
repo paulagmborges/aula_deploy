936d199e2ee1f72bda1070cfecfb6ef8413284e6
paula borges <paulabzambeli@gmail.com>
2023-11-22 08:13:55 -0300

    configuracao cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-	return res.json('Api está Ok!')
-})
+	try{
+		const carros = await knex('carros')
+		return res.json(carros)
+}catch(error){
+	console.log(error)
+	return res.status(500).json({mensagem:'Erro no servidor'})
+}
 
 const port = process.env.PORT || 3000
 
-app.listen(port,()=>{
-	console.log(`Servidor rodando na porta ${port}`)
-})
+app.listen(port, () => {
+	console.log(`Servidor em pé na porta ${port}`)
+}
+)
